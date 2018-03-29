b7199319c6ec6eae38129a8295c665fe6001aa0c
WangMeng <huangweiwen@ut.cn>
2018-03-29 10:27:17 +0800

    增加图像处理

---
+++ app/src/main/java/com/befiring/picturedemo/ImageHelper.java
@@ -0,0 +1,41 @@
+package com.befiring.picturedemo;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+
+/**
+ * Created by WangMeng on 2018/3/29.
+ */
+
+public class ImageHelper {
+
+    public static Bitmap handleImageEffect(Bitmap oriBmp, Bitmap bmp, float hue, float saturation, float lum) {
+        Canvas canvas = new Canvas(bmp);
+        Paint paint = new Paint();
+
+        ColorMatrix hueMatrix = new ColorMatrix();
+        hueMatrix.setRotate(0, hue);
+//        hueMatrix.setRotate(1, hue);
+//        hueMatrix.setRotate(2, hue);
+
+        ColorMatrix saturationMatrix = new ColorMatrix();
+        saturationMatrix.setSaturation(saturation);
+
+        ColorMatrix lumMatrix = new ColorMatrix();
+        lumMatrix.setScale(lum, lum, lum, 1);
+
+        ColorMatrix imageMatrix = new ColorMatrix();
+        imageMatrix.postConcat(hueMatrix);
+        imageMatrix.postConcat(saturationMatrix);
+        imageMatrix.postConcat(lumMatrix);
+
+        paint.setColorFilter(new ColorMatrixColorFilter(imageMatrix));
+        canvas.drawBitmap(oriBmp, 0, 0, paint);
+
+        return bmp;
+    }
+
+}
